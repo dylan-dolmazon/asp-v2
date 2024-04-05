e6d02a8c2d7d735d89abf44fc8524216a5830001
Dylan Dolmazon <ddolmazon@openstudio.fr>
2024-04-05 15:56:54 +0200

    [FRONTEND] delete .output

--- apps/frontend/.output/public/_nuxt/getPlayersRanking.DxD_1Y8T.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./useCustomFetch.CglE_PrC.js";const e=async(a,r=10)=>await s("players/ranking","GET",{params:{page:a,limit:r}});export{e as g};
--- apps/frontend/.output/public/_nuxt/getplayers.BTYGXSgH.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./useCustomFetch.CglE_PrC.js";const e=async(a,s=10)=>await r("players","GET",{params:{page:a,limit:s}});export{e as g};
